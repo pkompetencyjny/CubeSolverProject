cdfa491c96b7e4e893c96e1e7d34cf1b2b1a4cc8
Piotrek <Piotrek@DESKTOP-SMQ5BLL>
2018-11-06 11:31:05 +0100

    added extractColors.h

---
+++ src/maxind.h
@@ -0,0 +1,20 @@
+#ifndef MAXIND
+#define MAXIND
+ 
+#include <opencv\cv.hpp>
+ 
+int maxind(std::vector < int > v)
+{
+    int maxind = 0, max = 0, i = 0;
+    for (i = 0; i < 7; i++)
+    {
+        if (v[i] > max)
+        {
+            max = v[i];
+            maxind = i;
+        }
+    }
+    return maxind;
+}
+ 
+#endif
