34a415209ecf45a36d832b8f4536c10a158c5832
Piotrek <Piotrek@DESKTOP-SMQ5BLL>
2018-11-21 22:57:11 +0100

    Fixed communication with crop.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <thread>
+#include <string>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/core/core.hpp>
 #include "cam.hpp"
@@ -12,19 +13,20 @@ int main()
 	std::thread camThread(&camOpen);
 	char * stanKostki[6][9];
 	int j = 0;
-	int takNie;
+	char takNie='n';
 	std::system("CLS");
+	std::cout << "Wcisnij enter aby rozpoczac..." << std::endl;
 	if (std::getchar())
 	{
 		for (int i = 0; i < 6; i++)
 		{
 			do {
 				stanKostki[i][j] = vision();
-				std::cout << "Czy stan kostki siê zgadza?" << std::endl;
+				std::cout << "Czy stan sciany nr:"<<i+1<<" sie zgadza?(t/n)" << std::endl;
 				std::cin >> takNie;
-			} while (takNie == 1/* czy jakiœ inny true*/);
-			//}
+			} while (takNie != 't');
 			j += 9;
+			takNie = 'n';
 		}
 		CubeSolver cube = CubeSolver(stanKostki);
 		cube.solution();
